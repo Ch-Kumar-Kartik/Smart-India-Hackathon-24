39129c2bbf73c00ea99a3a68582d90886cd50034
Ch-Kumar-Kartik <143881752+Ch-Kumar-Kartik@users.noreply.github.com>
2024-09-03 14:04:13 +0530

    Update and rename app_sih.py to app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request jsonify
-
+from flask import Flask, render_template, request, jsonify
 from chat import get_response
 
 app = Flask(__name__)
@@ -11,11 +10,10 @@ def index_get():
 @app.post("/predict")
 def predict():
     text = request.get_json().get("message")
-    # TODO: check if text is valid
     response = get_response(text)
     message = {"answer": response}
     return jsonify(message)
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
+
